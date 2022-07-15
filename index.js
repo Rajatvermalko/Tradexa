51eb83f4f6ca85bd16fb01400a957333420362c7
Rajat Verma <104197270+Rajatvermalko@users.noreply.github.com>
2022-07-15 19:57:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const url = `https://jsonplaceholder.typicode.com/posts`;
+
+const getData = async () => {
+    let response = await fetch(url);
+    let data = await response.json();
+    for(let value of data ){
+        let createTableRow =  generateHTML(value.title,value.body);
+        document.getElementById('table').appendChild(createTableRow);
+    }   
+}
+
+
+const addData = ()=>{
+    let title = document.getElementById('title')
+    let body = document.getElementById('body')
+    let createTableRow = generateHTML(title.value,body.value);
+    document.getElementById('table').appendChild(createTableRow);
+}
+
+const generateHTML = (title,body) => {
+    let tr = document.createElement('tr');
+    let tdTitle = document.createElement('td');
+    let tdBody = document.createElement('td');
+    let titleNode = document.createTextNode(title);
+    let bodyNode = document.createTextNode(body);
+    tdTitle.appendChild(titleNode)
+    tdBody.appendChild(bodyNode)
+    tr.appendChild(tdTitle)
+    tr.appendChild(tdBody)
+    return tr
+}
